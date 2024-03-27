24e1b30dca5bdf16df0555288310604205aebeaa
SponeKiller <124689362+SponeKiller@users.noreply.github.com>
2024-03-27 09:35:49 +0100

    Update inicialize.py

--- model/inicialize.py
+++ model/inicialize.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import json
 from pathlib import Path
 from typing import Tuple
 
@@ -49,8 +50,8 @@ class Llama:
             torch.distributed.init_process_group("nccl")
 
         if not model_parallel_is_initialized():
-            if model_parallel_size is None:
-                model_parallel_size = 1
+            if config.model_parallel_size is None:
+                config.model_parallel_size = 1
             initialize_model_parallel(config.model_parallel_size)
 
         local_rank = int(os.environ.get("LOCAL_RANK", 0))
@@ -63,24 +64,25 @@ class Llama:
             sys.stdout = open(os.devnull, "w")
 
         start_time = time.time()
-        checkpoints = sorted(Path(ckpt_dir).glob("*.pth"))
-        assert len(checkpoints) > 0, f"no checkpoint files found in {ckpt_dir}"
-        assert model_parallel_size == len(
+        checkpoints = sorted(Path(config.ckpt_dir).glob("*.pth"))
+        assert len(checkpoints) > 0, f"no checkpoint files found in {config.ckpt_dir}"
+        assert config.model_parallel_size == len(
             checkpoints
-        ), f"Loading a checkpoint for MP={len(checkpoints)} but world size is {model_parallel_size}"
+        ), f"Loading a checkpoint for MP={len(checkpoints)} but world size is {config.model_parallel_size}"
         ckpt_path = checkpoints[get_model_parallel_rank()]
         checkpoint = torch.load(ckpt_path, map_location="cpu")
-        with open(Path(ckpt_dir) / "params.json", "r") as f:
-            params = json.loads(f.read())
 
+        
+        tokenizer = Tokenizer(model_path=config.tokenizer_path)
 
-        tokenizer = Tokenizer(model_path=tokenizer_path)
+        model_args = ModelArgs()
         model_args.vocab_size = tokenizer.n_words
         torch.set_default_tensor_type(torch.cuda.HalfTensor)
         model = Transformer(model_args)
         model.load_state_dict(checkpoint, strict=False)
+
         print(f"Loaded in {time.time() - start_time:.2f} seconds")
 
-        return model, tokenizer
+        return Tuple(model, tokenizer)
 
     
