5da32b968d9b7cb0139cc311bfca219681151304
SponeKiller <124689362+SponeKiller@users.noreply.github.com>
2024-04-11 23:43:18 +0200

    Updata

--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -1,5 +1,4 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
 
 import os
 from logging import getLogger
